24eafed0a105c706bec27b4f6654668b0013e584
KantaEndo <t236023@st.yamagata-u.ac.jp>
2024-10-18 19:06:44 +0900

    Add files via upload

---
+++ kadai2_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("What is the first number?");
+    scanf("%d",&a);
+	printf("What is the second number?");
+	scanf("%d",&b);
+	
+	c=a+b;
+    printf("The result of the addition is %d",c);
+  
+  return 0;
+}
